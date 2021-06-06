738543c18293085321945a7e719f6786730c621c
Nivea Gigen <60264386+ceccs18c43@users.noreply.github.com>
2021-06-06 10:51:39 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -5,7 +5,7 @@ function Note(){
   return (
     <div className="note">
          <h1>Javascript and React.js</h1>
-         <p>This was an amazing bootcamp taken up by Shaurya .We covered everything from scratch including javascript, React.js, HTML.</p>
+         <p>This was an amazing bootcamp taken up by Shaurya Sinha .We covered everything from scratch including javascript, React.js, HTML.</p>
     </div>
   );
 }
